31d33d4dbe6753af5b6bf193388cfcac9921f14d
jingqy <jingqy@mail.jj.cn>
2016-03-22 19:27:44 +0800

    todo

--- application/models/Picture_model.php
+++ application/models/Picture_model.php
@@ -8,8 +8,18 @@ class Picture_model extends CI_Model {
         $this->load->database();
     }
 
-    public function get_pic()
-    {
-    	return array();
-    }
+    //TODO 获取所有图片列表
+    public function getPictureList(){}
+
+    //TODO 获取图片详情
+    public function getPictureDetail( $intPId ){}
+
+    // TODO 根据标签类型取图片
+    public function getPictureListByTags( $intTId ) {}
+
+    //TODO 后台管理-图片分类
+    public function setPictureTagsId( $intPId, $intTId ){}
+
+
+
 }

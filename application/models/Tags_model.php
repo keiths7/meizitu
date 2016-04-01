b042186e52844d18fd811c658a646169e22b00d4
jingqy <jingqy@mail.jj.cn>
2016-04-01 15:10:26 +0800

    tags

---
+++ application/models/Tags_model.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Tags_model extends CI_Model {
+
+    public function __construct()
+    {
+        $this->load->database();
+    }
+
+    //TODO 获取分类列表
+    public function getTags(){}
+
+
+
+
+}
